8c4f4eec1fef86b2b5d800d44b4c6b79ab0224b5
Mark Kennedy <mark@gamer-network.net>
2014-11-03 21:12:27 +0000

    come on work goddammit

---
+++ csv2jsonp.py
@@ -0,0 +1,23 @@
+import webapp2
+
+import csv
+import urllib2
+import json
+
+url = 'http://images.eurogamer.net/2014/Mark/scorez.csv'
+response = urllib2.urlopen(url)
+cr = csv.reader(response)
+rows = []
+for row in cr:
+    rows.append(row)
+
+resp = json.dumps(rows)
+
+class CSV2JSONP(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(self.request.get("callback", default_value="__callback") + "(" + resp + ")")
+
+application = webapp2.WSGIApplication([
+    ('/', CSV2JSONP),
+], debug=True)
